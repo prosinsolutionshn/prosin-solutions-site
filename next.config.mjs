61f05914e129ff053e4be1b95daea61ef095b9b9
keven-bardales <122845766+keven-bardales@users.noreply.github.com>
2023-10-22 23:19:05 -0600

    feat(serverside): removed static site andd now serverside

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,6 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  cleanDistDir: true,
-  compiler: {
-    removeConsole: true,
-  },
   images: {
     remotePatterns: [
       {
@@ -16,9 +12,6 @@ const nextConfig = {
     ],
     unoptimized: true,
   },
-  output: 'export',
-  poweredByHeader: false,
-  swcMinify: true,
 };
 
 export default nextConfig;
